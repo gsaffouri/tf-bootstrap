e4f4335c9b6ecfe06899959b4c4a6c388c677ab4
Brian <b.matthew.chadwick@gmail.com>
2023-10-12 18:27:32 -0400

    Initial deployment

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.20.1"
+    }
+  }
+  required_version = "~> 1.5.0"
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "aws_s3_bucket" "backend" {
+    tags = {
+    Name        = "backend_bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  acl    = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.backend]
+}
