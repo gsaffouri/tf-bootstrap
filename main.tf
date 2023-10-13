c7a5134d4ce475f03e2f296bd17ca7f5c8b96509
Brian <b.matthew.chadwick@gmail.com>
2023-10-13 08:02:42 -0400

    Updated backend s3 bucket name for new account

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
   # Remote State File Configs Start
   # Uncomment and re-run after resources are created
-#   backend "s3" {
-#     bucket         = "terraform-20231013001906846700000001" # Update with bucket name after it is created
-#     key            = "state/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform_state"
-#   }
+  backend "s3" {
+    bucket         = "terraform-20231013120105412600000001" # Update with bucket name after it is created
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_state"
+  }
   # Remote State File Configs End
 }
 
