f93bfff1c800e9c2cbb0fdb7039be147fd7a3de5
Brian <b.matthew.chadwick@gmail.com>
2023-10-25 06:38:51 -0400

    Cleaning up

--- deploy-tf.sh
+++ deploy-tf.sh
@@ -26,9 +26,6 @@ done
 
 if [ -n "$argF" ]
 then
-  # Moving *.tf files to the root of the directory
-  mv resources/main-local-backend.tf .
-else
   # Copies main.tf file using local backend
   cp resources/main-local-backend.tf main.tf
 fi
